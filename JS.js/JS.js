f58690e4b9cb0ddc16078b31ab377c5afe1efe70
Yousef Aldeeb <129120120+yousefweb@users.noreply.github.com>
2025-09-24 23:44:50 +0300

    Update JS.js

--- JS.js/JS.js
+++ JS.js/JS.js
@@ -3,50 +3,19 @@ let isDown = false;
 let startX = 0;
 let scrollStart = 0;
 
-// ---- Mouse Events ----
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
-  slider.classList.add('active'); // optional for styling
   startX = e.pageX;
   scrollStart = slider.scrollLeft;
-  slider.style.cursor = 'grabbing';
 });
 
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-  slider.style.cursor = 'grab';
-});
-
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-  slider.style.cursor = 'grab';
+['mouseup', 'mouseleave'].forEach(event => {
+  slider.addEventListener(event, () => isDown = false);
 });
 
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
-  const move = e.pageX - startX;
+  let move = e.pageX - startX;
   slider.scrollLeft = scrollStart - move;
 });
-
-// ---- Touch Events (Mobile) ----
-slider.addEventListener('touchstart', (e) => {
-  isDown = true;
-  startX = e.touches[0].pageX;
-  scrollStart = slider.scrollLeft;
-});
-
-slider.addEventListener('touchend', () => {
-  isDown = false;
-});
-
-slider.addEventListener('touchmove', (e) => {
-  if (!isDown) return;
-  const move = e.touches[0].pageX - startX;
-  slider.scrollLeft = scrollStart - move;
-});
-
-// ---- Initial cursor style ----
-slider.style.cursor = 'grab';
