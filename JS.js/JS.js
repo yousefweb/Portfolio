69ab798e22b30044d275b11f2c1034bbd65c1fcf
Yousef Aldeeb <129120120+yousefweb@users.noreply.github.com>
2025-09-26 01:27:16 +0300

    Update JS.js

--- JS.js/JS.js
+++ JS.js/JS.js
@@ -1,24 +1,3 @@
-// const slider = document.querySelector('.education');
-// let isDown = false;
-// let startX = 0;
-// let scrollStart = 0;
-
-// slider.addEventListener('mousedown', (e) => {
-//   isDown = true;
-//   startX = e.pageX;
-//   scrollStart = slider.scrollLeft;
-// });
-
-// ['mouseup', 'mouseleave'].forEach(event => {
-//   slider.addEventListener(event, () => isDown = false);
-// });
-
-// slider.addEventListener('mousemove', (e) => {
-//   if (!isDown) return;
-//   e.preventDefault();
-//   let move = e.pageX - startX;
-//   slider.scrollLeft = scrollStart - move;
-// });
 document.addEventListener('DOMContentLoaded', () => {
   const slider = document.querySelector('.education');
   let isDown = false;
